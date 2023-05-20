e6649929307ccda7b27264700ee2afe6ccd1de39
Dalila997 <delilah99707@gmail.com>
2023-05-19 18:17:12 -0600

    first commit

---
+++ models/Model.js
@@ -0,0 +1,16 @@
+//importamos la conexion a la bd
+import db from "../database/db.js"
+import { DataTypes } from "sequelize"
+
+const AutoModel = db.define('autos',{
+    nombre: {type:DataTypes.STRING},
+    marca: {type:DataTypes.STRING},
+    modelo: {type:DataTypes.NUMBER},
+    precio: {type:DataTypes.FLOAT}
+
+},{
+    timestamps: false
+}
+)
+
+export default AutoModel
