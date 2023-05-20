3dc69450e64621b1d74ef742fb960d813eb5c3b7
Dalila997 <delilah99707@gmail.com>
2023-05-19 18:52:31 -0600

    src

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from 'cors'
-import db from "./database/db.js";
-import autoRoutes from "./routes/routes.js"
+import db from "./src/database/db.js";
+import autoRoutes from "./src/routes/routes.js"
 
 const app = express()
 
