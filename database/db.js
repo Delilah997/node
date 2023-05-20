5f19a5e1ef5b71187f9b70fe2f0112cd765c99d4
Dalila997 <delilah99707@gmail.com>
2023-05-19 18:38:07 -0600

    movimiento exitoso

--- database/db.js
+++ database/db.js
@@ -1,7 +1,8 @@
 import {Sequelize} from 'sequelize'
 
-const db = new Sequelize('database_app', 'root', '',{
-    host: 'localhost',
+const db = new Sequelize('database_app', 'root', 'uS2ppKTTChILynQHk4TZ',{
+    host: 'containers-us-west-105.railway.app',
+    port: '5549',
     dialect: 'mysql'
 })
 
